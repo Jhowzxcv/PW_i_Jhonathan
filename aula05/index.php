2ab9d72c92b5b81d176c029f88af984a5a9bb5a9
Jhowzxcv <jhodourado131@gmail.com>
2024-08-11 19:27:12 -0300

    teste

---
+++ aula05/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicios</title>
+    <style>
+        p {
+            height: 25px;
+        }
+    </style>
+</head>
+<body>
+    <pre>
+
+        <?php
+
+        $n1=1;
+        $n2=2;
+
+        function soma($n1,$n2) {
+            $soma = $n1 + $n2;
+            return $soma;
+        }
+
+
+
+        ?>
+
+
+    </pre>
+</body>
+</html>
